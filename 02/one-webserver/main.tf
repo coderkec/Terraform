589e2267b4b3df3b3b0c9e3ad9439eeaad74d060
coderkec <noody123@naver.com>
2025-12-11 18:01:21 +0900

    2nd commit message line

--- 02/one-webserver/main.tf
+++ 02/one-webserver/main.tf
@@ -1,12 +1,12 @@
-###############################################
-# 1. provider
-# 2. EC2 Instance 생성
-# * 1 EC2 create 
-# * 2 
-###############################################
+###################################
+# 1. terraform/provider
+# 2. EC2 Instance 생성(user_data(WEB:8080))
+# * 1) SG(8080)
+# * 2) EC2 생성
+###################################
 
 #
-# provider
+# 1. terraform/provider
 #
 terraform {
   required_providers {
@@ -21,45 +21,48 @@ provider "aws" {
   region = "us-east-2"
 }
 
-# 
-# resource
 #
-resource "aws_instance" "myinstance" {
-  ami                    = "ami-0f5fcdfbd140e4ab7"
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_8080.id]
+# 2. resource - EC2 인스턴스 생성
+#
 
-  user_data_replace_on_change = true
-  user_data                   = <<EOF
-#!/bin/bash
-echo "Hello, World" > index.html
-nohup busybox httpd -f -p 8080 &
-EOF
-  tags = {
-    Name = "MyFirstInstance"
-  }
-}
-
+# 1) SG 생성
 resource "aws_security_group" "allow_8080" {
   name        = "allow_8080"
-  description = "Allow 8080 inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
+  description = "Allow TLS inbound traffic and all outbound traffic"
 
   tags = {
     Name = "allow_8080"
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_8080_httpd" {
+resource "aws_vpc_security_group_ingress_rule" "allow_8080_http" {
   security_group_id = aws_security_group.allow_8080.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 8080
-  ip_protocol       = "tcp"
   to_port           = 8080
+  ip_protocol       = "tcp"
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allow_8080.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+  ip_protocol       = "-1"
+}
+
+# 2) EC2 생성
+resource "aws_instance" "myinstance" {
+  ami                    = "ami-0f5fcdfbd140e4ab7"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_8080.id]
+
+  user_data_replace_on_change = true
+  user_data                   = <<-EOF
+        #!/bin/bash
+        echo "Hello World" > index.html
+        nohup busybox httpd -f -p 8080 &
+        EOF
+
+  tags = {
+    Name = "My-First-Instance"
+  }
 }
