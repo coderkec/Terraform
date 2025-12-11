b172166e176ffa5fb68fbb9c20ff9d717073a925
coderkec <noody123@naver.com>
2025-12-11 17:06:45 +0900

    Add webserver configuration

---
+++ 02/one-webserver/main.tf
@@ -0,0 +1,65 @@
+###############################################
+# 1. provider
+# 2. EC2 Instance 생성
+# * 1 EC2 create 
+# * 2 
+###############################################
+
+#
+# provider
+#
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.25.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+# 
+# resource
+#
+resource "aws_instance" "myinstance" {
+  ami                    = "ami-0f5fcdfbd140e4ab7"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_8080.id]
+
+  user_data_replace_on_change = true
+  user_data                   = <<EOF
+#!/bin/bash
+echo "Hello, World" > index.html
+nohup busybox httpd -f -p 8080 &
+EOF
+  tags = {
+    Name = "MyFirstInstance"
+  }
+}
+
+resource "aws_security_group" "allow_8080" {
+  name        = "allow_8080"
+  description = "Allow 8080 inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "allow_8080"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_8080_httpd" {
+  security_group_id = aws_security_group.allow_8080.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 8080
+  ip_protocol       = "tcp"
+  to_port           = 8080
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_8080.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
